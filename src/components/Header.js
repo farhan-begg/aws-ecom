fc969e5faa07d112289c3b8bdb4653ec3dc1891f
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-01-07 18:35:45 -0800

    update navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <header className="main-head">
+        <div className="main-head">
             <nav>
-                <h1 id="logo">Wisdomly</h1>
-                <ul>
-                    <li>
+                <h1 id="logo">Noble Cause</h1>
+                <ul className="link">
+                    <li >
                         <Link to="/">Home</Link>
                     </li>
-                    <li>
+                    <li >
                         <Link to="/books">Books</Link>
                     </li>
-                    <li>
+                    <li >
                         <Link to="/cart">Cart</Link>
                     </li>
-                    <li>
+                    <li >
                         <Link to="/checkout">Checkout</Link>
                     </li>
                 </ul>
             </nav>
-        </header>
+        </div>
     )
 }
 
